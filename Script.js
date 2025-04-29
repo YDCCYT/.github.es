10514e16fe6429f2a0d035e6adc69aa2b2678d3d
YDCCYT <yeisoncuesta.ineaco@gmail.com>
2025-04-29 17:14:44 -0500

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,8 @@
+function Next() {
+    var x = document.querySelector(".ydcc");
+    var y = document.querySelector(".mariana");
+    var color = document.querySelector("body");
+    color.style.background = "radial-gradient(circle, #fcf75e, #f8f32b)";
+    x.style.display = "none";
+    y.style.display = "block";
+}
