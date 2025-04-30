5fe518b58762fd90d57643c376f06844f68022e8
YDCCYT <yeisoncuesta.ineaco@gmail.com>
2025-04-30 12:29:12 -0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,8 +1,36 @@
 function Next() {
     var x = document.querySelector(".ydcc");
     var y = document.querySelector(".mariana");
+    var z = document.querySelector(".jhorman");
     var color = document.querySelector("body");
     color.style.background = "radial-gradient(circle, #fcf75e, #f8f32b)";
     x.style.display = "none";
     y.style.display = "block";
+    if (x,y == "none") {
+        z.style.display = "block";
+    }
+}    
+
+function Next2() {
+    var x = document.querySelector(".ydcc");
+    var y = document.querySelector(".mariana");
+    var z = document.querySelector(".jhorman");
+    var color = document.querySelector("body");
+    color.style.background = "radial-gradient(circle, #122562, #50C878)";
+    x.style.display = "none";
+    y.style.display = "none";
+    z.style.display = "block";
+}
+
+function frase() {
+    var x = document.querySelector(".ydcc");
+    var y = document.querySelector(".mariana");
+    var z = document.querySelector(".jhorman");
+    var a = document.querySelector(".frase");
+    var color = document.querySelector("body");
+    color.style.background = "radial-gradient(circle, #122562, #50C878)";
+    x.style.display = "none";
+    y.style.display = "none";
+    z.style.display = "none";
+    a.style.display = "block";
 }
